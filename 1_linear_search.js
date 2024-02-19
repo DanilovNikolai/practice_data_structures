13be880221a85904193f901c47e0423db2229a76
Danilov_Nikolai <dan1k91@mail.ru>
2024-02-19 21:47:17 +0300

    linear_search

--- 1_linear_search.js
+++ 1_linear_search.js
@@ -1,1 +1,17 @@
+//!Линейный поиск, сложность О(n)
 
+const array = [1, 4, 3, 10, 66, 42, 7, 14, 18, 5, 99, 13];
+let count = 0;
+
+const linearSearch = (array, item) => {
+  for (let i = 0; i < array.length; i++) {
+    count++;
+    if (array[i] === item) {
+      return "Найдено число на позиции с индексом " + i;
+    }
+  }
+  return "Число не найдено!";
+};
+
+console.log(linearSearch(array, 42));
+console.log("Число операций - ", count);
